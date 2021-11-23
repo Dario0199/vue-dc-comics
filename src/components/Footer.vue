f6e06e8ca61dfc97dd9966c1e7f57f947e79c679
Dario0122 <dario.di.cuia@gmail.com>
2021-11-23 17:45:53 +0100

    logo footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,52 @@
+<template>
+    <footer>
+        <div class="container">
+            <div class="left">
+                left
+            </div>
+            <div class="right">
+                <img src="../assets/dc-logo-bg.png" alt="dc logo footer">
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style scoped lang="scss">
+    footer{
+        max-width: 100%;
+        background-image: url('../assets/footer-bg.jpg');
+        background-size: cover;
+        padding: 20px 0;
+    
+        .container{
+        display: flex;
+        width: 80%;
+        margin: 0 auto;
+
+            .left,
+            .right{
+                width: 50%;
+                img{
+                    width: 450px;
+                }
+            }
+
+            .right{
+                display: flex;
+                justify-content: flex-end;
+            }
+        }
+
+    }
+
+        
+       
+
+
+</style>
