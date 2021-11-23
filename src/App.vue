9b44ac30d10decb6fcf2af04fe963514fe5b68ab
Dario0122 <dario.di.cuia@gmail.com>
2021-11-23 16:18:09 +0100

    header

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,35 @@
 <template>
-  <div id="app">vue dc comics</div>
+  <div id="app">
+    <!-- header -->
+    <Header />
+
+    <!-- main -->
+    <main></main>
+
+    <!-- footer -->
+    <footer></footer>
+  </div>
 </template>
 
 <script>
-
+import  Header from'@/components/Header.vue';
 
 export default {
   name: 'App',
   components: {
-    
+      Header,
   }
 }
 </script>
 
 <style lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Header.vue
@@ -0,0 +1,82 @@
+<template>
+    <header>
+        <div>
+            <a href="/">
+                <img src="../assets/dc-logo.png" alt="dc comics">
+            </a>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/">character</a>
+                    </li>
+                    <li>
+                        <a href="/">comics</a>
+                    </li>
+                    <li>
+                        <a href="/">movie</a>
+                    </li>
+                    <li>
+                        <a href="/">tv</a>
+                    </li>
+                    <li>
+                        <a href="/">games</a>
+                    </li>
+                    <li>
+                        <a href="/">collectibles</a>
+                    </li>
+                    <li>
+                        <a href="/">videos</a>
+                    </li>
+                    <li>
+                        <a href="/">fans</a>
+                    </li>
+                    <li>
+                        <a href="/">news</a>
+                    </li>
+                    <li>
+                        <a href="/">shop</a>
+                    </li>
+                </ul>
+            </nav>
+
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'header',
+}
+</script>
+
+<style scoped lang="scss">
+div{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    max-width: 80%;
+    margin: 0 auto;
+    
+    img{
+        width: 70px;
+    }
+
+    ul{
+        display: flex;
+        justify-content: flex-end;
+
+        li{
+            list-style: none;
+
+            a{
+                text-decoration: none;
+                padding: 1rem;
+                text-transform: uppercase;
+                font-size: 0.60rem;
+                font-weight: 700;
+            }
+        }
+    }
+}
+ 
+</style>
